a5ff839747eaa42dafb1d0a18a493048bae15cb9
Jacob Hummer <jcbhmr@outlook.com>
2024-04-14 01:25:57 +0000

    saveve

---
+++ task.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Inspired by https://github.com/adriancooney/Taskfile
+set -e
+if [[ -n $DEBUG ]]; then set -x; fi
+
+setup() (
+    cd WebKit
+    if [[ $(uname -s) == "Linux" ]]; then
+        if [[ $(lsb_release -is) == "Ubuntu" ]]; then
+            sudo apt install libicu-dev python ruby bison flex cmake build-essential ninja-build git gperf
+            sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+        else
+            echo dunno
+            exit 1
+        fi
+    elif [[ $(uname -s) == "Darwin" ]]; then
+        echo dunno
+        exit 1
+    elif [  "$OS" = "Windows_NT" ]; then
+        echo dunno
+        exit 1
+    else
+        echo dunno
+        exit 1
+    fi
+)
+
+build() (
+    cd WebKit
+    CC=clang-18 CXX=clang++-18 Tools/Scripts/build-webkit --jsc-only --cmakeargs="-DENABLE_STATIC_JSC=ON -DUSE_THIN_ARCHIVES=OFF"
+    ln -sf "$PWD"/WebKitBuild/JSCOnly/Release "$(cd .. && pwd)/dist"
+)
+
+$@
